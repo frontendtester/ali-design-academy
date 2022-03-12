a11cc6a9d9cd42e8bec9ce175cfdf8cd19f0890e
frontendtester <frontendtester@yandex.ru>
2022-03-12 12:12:05 +0500

    Comment out JS form submission

--- js/main.js
+++ js/main.js
@@ -8,30 +8,30 @@ if (elSiteHeaderToggler) {
 }
 
 
-const elRegistrationForm = document.querySelector('.registration-form');
+// const elRegistrationForm = document.querySelector('.registration-form');
 
-if (elRegistrationForm) {
-  elRegistrationForm.addEventListener('submit', function (evt) {
-    evt.preventDefault();
+// if (elRegistrationForm) {
+//   elRegistrationForm.addEventListener('submit', function (evt) {
+//     evt.preventDefault();
 
-    let data = {
-      Ism: elRegistrationForm.querySelector('[name="Ism"]').value,
-      Telefon: elRegistrationForm.querySelector('[name="Telefon"]').value,
-      Kurs: elRegistrationForm.querySelector('[name="Kurs"]').value
-    };
+//     let data = {
+//       Ism: elRegistrationForm.querySelector('[name="Ism"]').value,
+//       Telefon: elRegistrationForm.querySelector('[name="Telefon"]').value,
+//       Kurs: elRegistrationForm.querySelector('[name="Kurs"]').value
+//     };
 
-    fetch('https://script.google.com/macros/s/AKfycbwm6KDq2w1P_j6UzAerEc9Vgg3arErjd8uXEH78M-Lzxt22mVR6K5abqnhpzu_3TwzU/exec', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    });
-  });
-}
+//     fetch('https://script.google.com/macros/s/AKfycbwm6KDq2w1P_j6UzAerEc9Vgg3arErjd8uXEH78M-Lzxt22mVR6K5abqnhpzu_3TwzU/exec', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json;charset=utf-8'
+//       },
+//       body: JSON.stringify(data)
+//     })
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//     });
+//   });
+// }
